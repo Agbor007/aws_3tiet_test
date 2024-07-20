7fd15318bd4fc179f711c5ff70133102e146ba67
synthetico <33494606+synthetico@users.noreply.github.com>
2024-07-20 23:04:18 +0100

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -2,5 +2,5 @@ module.exports = Object.freeze({
     DB_HOST : 'YOUR-DATABASE-ENDPOINT.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'YourPassword',
-    DB_DATABASE : 'webappdb'
+    DB_DATABASE : 'trustechdb'
 });
